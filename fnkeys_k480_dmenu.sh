577ae5f5dcacfdf2af6173de4ef379664c6f67d1
Davi Mendes <dmendes@aluno.unb.br>
2020-05-12 13:55:59 -0300

    dmenu script to toggle Functions Keys

--- fnkeys_k480_dmenu.sh
+++ fnkeys_k480_dmenu.sh
@@ -8,7 +8,7 @@ DEV="$(ls /sys/class/hidraw/ -l | grep 046D:B33D | grep -o 'hidraw[0-9]*$')"
 
 if test -n "$DEV" && (test -z "$1" || test "/dev/$DEV" = "$1")
 then
-    sudo $TOOL -d "/dev/$DEV" -f $IN
+    $TOOL -d "/dev/$DEV" -f $IN
 else
     notify-send "FN-Keys Errror: K480 not found!"
 fi
